3f5e37a42dd3cca63a0bc38a69d736a61444eb55
Cavid2002 <quliyevcavid132@gmail.com>
2022-10-28 11:39:53 +0400

    email recovery done

--- controls.py
+++ controls.py
@@ -1,13 +1,21 @@
-from flask import render_template,redirect,make_response,url_for,flash,request
+from flask import render_template,redirect,make_response,url_for,flash,request,session
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import current_user,login_required,login_user,logout_user
 from werkzeug.utils import secure_filename
 from flask_mail import Message
 from app import app,photos,login_manger,video,mail
 from forms import LoginForm,SignUpForm,UploadImage,UploadVideo,PasswordRecoverForm
+from flask_uploads import UploadNotAllowed
 from uuid import uuid4
-from os import remove
+from datetime import timedelta
+from os import remove,urandom
 from models import *
+
+
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(days=2)
 
 
 @login_manger.user_loader
@@ -22,15 +30,27 @@ def recover():
     if(rform.validate_on_submit()):
         em = rform.email.data
         user = User.query.filter_by(email = em).first()
-        if(user != None):
-            msg = Message("Email Recovery",recipients=[em])
+        if(user):
+            token = urandom(20).hex()
+            session['email'] = em
+            session['token'] = token
+            msg = Message(f"Email Recovery:",recipients=[em])
+            msg.body = f"Email Recovery token is:http://127.0.0.1:5000/recoverinfo/{token} \n Do not share with anyone!\n if it wasn't you ignore this message"
             mail.send(msg)
-            return redirect(url_for('recover'))
         else:
             flash("User with this email doesn't exists!")
     res = make_response(render_template("recover.html",form = rform))
     return res
 
+@app.route("/recoverinfo/<tk>")
+def recoverCheck(tk):
+    if('token' in session and tk == session['token']):
+        user = User.query.filter_by(email = session['email']).first()
+        login_user(user)
+        session.pop('token')
+        session.pop('email')
+        return redirect(url_for('main'))
+    return make_response("<h2>Access Denied</h2>",403)
     
 @app.route("/main/images",methods = ['GET','POST'])
 @login_required
@@ -45,10 +65,13 @@ def main():
             current_user.profile = splitprofile[1]
             current_user.save()
         if(formImg.img.data):
-            file = photos.save(storage=formImg.img.data,folder=current_user.folder)
-            spiltname = file.split("/")
-            new_img = Images(img_path=spiltname[1],user_id=current_user.id)
-            new_img.save()
+            try:
+                file = photos.save(storage=formImg.img.data,folder=current_user.folder)
+                spiltname = file.split("/")
+                new_img = Images(img_path=spiltname[1],user_id=current_user.id)
+                new_img.save()
+            except UploadNotAllowed:
+                flash("Make sure to upload Image file") 
         return redirect(url_for('main'))
     userImg = Images.query.filter_by(user_id = current_user.id)
     res = make_response(render_template("mainImg.html",user_info = current_user,form = formImg,images = userImg))
@@ -60,11 +83,6 @@ def main():
 def videos():
     formVideo = UploadVideo()
     if(formVideo.validate_on_submit()):
-        if(formVideo.video.data):
-            file = video.save(storage = formVideo.video.data,folder=current_user.folder)
-            spiltname = file.split("/")
-            new_video = Videos(video_path=spiltname[1],user_id=current_user.id)
-            new_video.save()
         if(formVideo.profile.data):
             if(current_user.profile != "person-icon.png"):
                 remove("static/uploads/"+current_user.folder + "/" + current_user.profile)
@@ -72,6 +90,15 @@ def videos():
             splitprofile = profile.split("/")
             current_user.profile = splitprofile[1]
             current_user.save()
+            
+        if(formVideo.video.data):
+            try:
+                file = video.save(storage = formVideo.video.data,folder=current_user.folder)
+                spiltname = file.split("/")
+                new_video = Videos(video_path=spiltname[1],user_id=current_user.id)
+                new_video.save()
+            except UploadNotAllowed:
+                flash("Make sure to upload Video file!")
         return redirect(url_for('videos'))
     userVid = Videos.query.filter_by(user_id = current_user.id)
     res = make_response(render_template("mainVideos.html",user_info = current_user,form = formVideo,vids = userVid))
