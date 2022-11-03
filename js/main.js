611da1c40c0606287575ec33e1578b4e121b1c15
Elmir <elmir.quluzade627@gmail.com>
2022-11-03 23:33:53 +0400

    signing ready

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 const dropdown = document.querySelector(".dropdown");
 const dropdownIcon = document.querySelector("#dropdownIcon");
+const burger = document.querySelector(".burger");
+const burgerList = document.querySelector(".burgerList");
+const line1 = document.querySelector(".line1");
+const line2 = document.querySelector(".line2");
+const line3 = document.querySelector(".line3");
+
+
+burger.addEventListener("click", () => {
+    line1.classList.toggle("linecross1");
+    line2.classList.toggle("linecross2");
+    line3.classList.toggle("linecross3");
+    burgerList.classList.toggle("active");
+    if (dropdown.classList.contains("active")) {
+        dropdown.classList.remove("active");
+        dropdownIcon.innerHTML = "arrow_drop_down";
+    }
+});
 
 const dropdownOpen = () => {
     dropdown.classList.toggle("active");
+    if (burgerList.classList.contains("active")) {
+        line1.classList.remove("linecross1");
+        line2.classList.remove("linecross2");
+        line3.classList.remove("linecross3");
+        burgerList.classList.remove("active");
+    }
     if (dropdownIcon.innerHTML == "arrow_drop_up") {
         dropdownIcon.innerHTML = "arrow_drop_down";
     } else {
@@ -10,22 +33,15 @@ const dropdownOpen = () => {
     }
 };
 
-const dropdownClose = () => {
+const menusClose = () => {
     if (dropdown.classList.contains("active")) {
         dropdown.classList.remove("active");
         dropdownIcon.innerHTML = "arrow_drop_down";
     }
+    if (burgerList.classList.contains("active")) {
+        line1.classList.remove("linecross1");
+        line2.classList.remove("linecross2");
+        line3.classList.remove("linecross3");
+        burgerList.classList.remove("active");
+    }
 };
-
-const burger = document.querySelector('.burger');
-burger.addEventListener('click', () => {
-    const burgerList = document.querySelector('.burgerList');
-    const line1 = document.querySelector('.line1');
-    const line2 = document.querySelector('.line2');
-    const line3 = document.querySelector('.line3');
-    line1.classList.toggle('linecross1');
-    line2.classList.toggle('linecross2');
-    line3.classList.toggle('linecross3');
-    burgerList.classList.toggle('active');
-
-})
