02222f0c481ab2a3162287d1caa8f6478ea578e2
Elmir <elmir.quluzade627@gmail.com>
2022-11-14 23:12:55 +0400

    comment

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const line1 = document.querySelector(".line1");
 const line2 = document.querySelector(".line2");
 const line3 = document.querySelector(".line3");
 const likeBtn = document.querySelectorAll(".like-icon");
+const likeCount = document.querySelectorAll(".post-likes");
 const commentBtn = document.querySelectorAll(".comment-icon");
 
 burger.addEventListener("click", () => {
@@ -50,14 +51,8 @@ const menusClose = () => {
 document.addEventListener("click", (e) => {
     for (let item of likeBtn) {
         if (item == e.target) {
-            const className = item.className.split(' ');
             item.classList.toggle("fa-solid");
-            if(className[1] == "fa-heart"){
-                item.classList.toggle("likeColor");
-            }
-            if(className[1] == "fa-bookmark"){
-                item.classList.toggle("bookmarkColor");
-            }
+            item.classList.toggle("likeColor");
         }
     }
 });
@@ -65,7 +60,9 @@ document.addEventListener("click", (e) => {
 document.addEventListener("click", (e) => {
     for (let item of commentBtn) {
         if (item == e.target) {
-            console.log('first')
+            location.href = "../comment.html"
         }
     }
 });
+
+
